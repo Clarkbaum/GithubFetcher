19227c11549e3d928c02dcb88dbe1cdab13c6034
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-03-06 15:01:34 -0600

    good progress with github api

--- README.md
+++ README.md
@@ -42,7 +42,7 @@ Here are diagrams of the above two main points:
 
 - [ /] Use [knex migrations](http://knexjs.org/#Migrations-CLI) to create a `repos` table. It should have columns for the repo's name, the owner's username, and the repo's number of stargazers.
 
-- [ ] When a user types a github username into the text field, use jQuery to fetch that user's GitHub repositories from the [GitHub API](https://developer.github.com/v3/).
+- [ /] When a user types a github username into the text field, use jQuery to fetch that user's GitHub repositories from the [GitHub API](https://developer.github.com/v3/).
 
 - [ ] Still using jQuery, send the data you get from GitHub to your express server via `POST /repos/import`. This endpoint should store that data in the database.
 
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var app = express();
 module.exports = app;
 
+app.use(bodyParser.urlencoded());
 app.use( bodyParser.json() );
 
 
@@ -17,17 +18,20 @@ var knex = require('knex')({
 
 
 app.post('/repos/import', function (req, res) {
-  // TODO
+  console.log("req.body", req.body)
+  res.status(200);
+  res.end("test end");
 });
 
 
 app.get('/repos', function (req, res) {
-  // TODO
+  console.log("get /repos")
+  res.status(200);
+  res.end("test end")
 });
 
 
 app.get('/', function (req, res) {
-  console.log("dirname", __dirname)
   //res.sendFile(__dirname + '/client/index.html')
   //not sure why this __dirname isnt working oh well
   res.sendFile('/Users/nbaumg/fullstack-exercise/hratx25-fullstack-exercise/client/index.html')
